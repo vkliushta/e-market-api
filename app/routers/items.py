7cfad5df62ecb89bcc195772da6b14e24bb85250
Vasyl <vasyl.kliushta@gmail.com>
2022-03-06 15:36:36 +0200

    reformat crud operations

--- app/routers/items.py
+++ app/routers/items.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from .. import crud, schemas
+from .. import schemas
+from ..crud import items
 from ..database import get_db
 
 router = APIRouter(
@@ -13,5 +14,14 @@ router = APIRouter(
 
 @router.get("/", response_model=list[schemas.Item])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
+    all_items = items.get_items(db, skip=skip, limit=limit)
+    return all_items
+
+
+@router.post("/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(
+    user_id: int,
+    item: schemas.ItemCreate,
+    db: Session = Depends(get_db),
+):
+    return items.create_user_item(db=db, item=item, user_id=user_id)
